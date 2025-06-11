fb4f10236890a76dc7bc94cb3b8349ed38dec4e5
cristian <cgonzalez@gpconsultores.cl>
2025-06-11 15:54:30 -0400

    cambiar id estacion en test

--- test_registro_alerta.py
+++ test_registro_alerta.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from registro_alertas import registrar_alarma_persistente
 
 sensor_id = 157
-estacion_id = 999  # ⚠️ reemplaza por el `estacion_id` real de este sensor en tu BD
+estacion_id = 124  # ⚠️ reemplaza por el `estacion_id` real de este sensor en tu BD
 fecha = datetime.now() + timedelta(seconds=1)  # fuerza un timestamp nuevo
 valor = 999
 criterio = 3
