523995fe888d51123f503231934f73fc6f996bcd
cristian <cgonzalez@gpconsultores.cl>
2025-06-17 10:33:16 -0400

    Eliminados archivos de pruebas innecesarios

--- test_conexion.py
+++
@@ -1,9 +0,0 @@
-# test_conexion.py
-from sqlalchemy import text
-from conexion import engine
-try:
-	with engine.connect() as conn:
-		result = conn.execute(text("SELECT 1"))
-		print("Conexion exitosa:", result.scalar()) 
-except Exception as e:
-	print("Error al conectar:", e)
