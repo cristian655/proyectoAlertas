4a55c6ed361da4efa938562f8f853a4e4d251fd0
cristian <cgonzalez@gpconsultores.cl>
2025-07-30 09:33:51 -0400

    Actualizar usuarios 2

--- enviar_correo.py
+++ enviar_correo.py
@@ -19,9 +19,9 @@ SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
 
 DESTINATARIOS_POR_DEFECTO = [
-    "cgonzalez@gpconsultores.cl"#,
-    #"erivas@gpconsultores.cl",
-    #"hjilberto@gpconsultores.cl"
+    "cgonzalez@gpconsultores.cl",
+    "hjilberto@gpconsultores.cl"#,
+    #"erivas@gpconsultores.cl"
 ]
 DESTINATARIOS_VICTOR = DESTINATARIOS_POR_DEFECTO
 UMBRAL_ENVIO_REPETICION = 3
